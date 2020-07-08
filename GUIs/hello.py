4b67c25415bacbf540925977c9a60d19c37c51af
naveed <imnaveed2003@gmail.com>
2020-07-08 12:56:35 +0400

    added

---
+++ GUIs/hello.py
@@ -0,0 +1,20 @@
+import os
+files = [
+    "admission_menu",
+    "fee_deposit",
+    "fee_menu",
+    "LOGIN_PAGE",
+    "main_menu",
+    "new_user_page",
+    "student_data",
+    "ADD_BOOK",
+    "library_menu",
+    "fee_details"
+]
+cmd = "pyuic5 -x "
+
+for i in files:
+    temp = cmd +"GUIs/"+ i + ".ui -o " + i + ".py"
+    print(temp)
+    os.system(temp)
+    temp = ''
